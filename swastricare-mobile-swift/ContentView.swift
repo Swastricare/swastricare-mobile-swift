1a11538af23dc55e58dc9a2d482373672385d117
nikhil8182 <ceo@Onwords.in>
2026-01-06 18:02:48 +0530

    Initial Commit

---
+++ swastricare-mobile-swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swastricare-mobile-swift
+//
+//  Created by Nikhil Deepak on 06/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swastricare-mobile-swift/swastricare_mobile_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  swastricare_mobile_swiftApp.swift
+//  swastricare-mobile-swift
+//
+//  Created by Nikhil Deepak on 06/01/26.
+//
+
+import SwiftUI
+
+@main
+struct swastricare_mobile_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
